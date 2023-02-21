e51504635772ae703ab5d52c6516ef06096ae8b1
ad3rinto <aad3rinto@gmail.com>
2023-02-21 16:46:28 +0000

    solved first/simple rect state problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import Starter from "./tutorial/01-useState/starter/01-error-example"
+import { useState } from "react";
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const change = () => {
+    setCount(count + 1)
+  }
+
+
+
+
+  // const increaseNumber = () => {
+  //   count = count++
+  //   console.log(count)
+  // }
+
   return (
     <div className='container'>
       <h2>Advanced React</h2>
+      <Starter />
+      <h3>{count}</h3>
+      <button onClick={change}>Click Me</button>
     </div>
   );
 }
