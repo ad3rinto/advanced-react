80f3e02cd5cf4d9be6a39a84016e5666760397a5
ad3rinto <aad3rinto@gmail.com>
2023-02-22 14:19:10 +0000

    array example work

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import Starter from "./tutorial/01-useState/starter/01-error-example"
+import Starter from "./tutorial/01-useState/starter/03-useState-array"
 import { useState } from "react";
+import { data } from "../../03-advanced-react/src/data";
 
+console.log(data)
 function App() {
 
-  const [count, setCount] = useState(0);
-
-  const change = () => {
-    setCount(count + 1)
-  }
+  const [people, setPeople] = useState(data);
 
 
 
 
-  // const increaseNumber = () => {
-  //   count = count++
-  //   console.log(count)
-  // }
+
+
 
   return (
     <div className='container'>
       <h2>Advanced React</h2>
       <Starter />
-      <h3>{count}</h3>
-      <button type="button" onClick={change} className="btn">Click Me</button>
+      {people.map((person) => {
+        return <h3> {person.name} </h3>
+
+      })}
+
+
+
+
     </div>
   );
 }
