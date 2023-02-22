0022e9ec852814b68d3f9a6f81605425ed88db43
ad3rinto <aad3rinto@gmail.com>
2023-02-22 21:03:18 +0000

    add remove single item and remove all item methods for list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Starter from "./tutorial/01-useState/starter/03-useState-array"
 import { useState } from "react";
-import { data } from "../../03-advanced-react/src/data";
+import { data } from "./data";
 
 console.log(data)
 function App() {
@@ -11,7 +11,8 @@ function App() {
     setPeople([])
   }
   const removeItem = (id) => {
-    console.log(id)
+    const newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
   }
 
 
