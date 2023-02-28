d35b3b132c001a1b8488e45f0afaaea51843b680
ad3rinto <aad3rinto@gmail.com>
2023-02-28 13:19:09 +0000

    fixed rendering issue by creating new perso object

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ import { useState } from "react";
 
 export function App() {
 
+  const [person, setPerson] = useState({
+    name: "segun Agbaje",
+    age: 54,
+    hobby: "Skin diving"
+  })
 
+  const updateDetails = () => {
+    setPerson({
+      name: "Adeyemo Akanbi",
+      age: 2023 - 1940,
+      hobby: " Preaching the word of God"
+    })
+  }
 
 
 
@@ -13,11 +25,11 @@ export function App() {
     <div className='container'>
       <Starter />
       <div>
-        <h1>Captain Ajasin</h1>
-        <h2>50</h2>
-        <p>Enjoys: tibi</p>
+        <h1>{person.name}</h1>
+        <h2>{person.age}</h2>
+        <p>Enjoys:{person.hobby}</p>
       </div>
-      <button type="button" className="btn" >Click Me</button>
+      <button type="button" className="btn" onClick={updateDetails} >Click Me</button>
 
     </div>
   );
