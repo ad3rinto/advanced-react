6e699eeb360e4817a9140cbbfe2d346e197c295e
ad3rinto <aad3rinto@gmail.com>
2023-03-01 18:01:32 +0000

    added some more data to the person object

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,30 @@ import { useState } from "react";
 export function App() {
 
   const [person, setPerson] = useState({
-    name: "segun Agbaje",
+    name: "Dominic Longstaff",
     age: 54,
-    hobby: "Skin diving"
+    hobby: "Skin diving",
+    occupation: "Actor",
+    nextOfKin: "Mariam Makeba",
+    securityClearance: true
   })
 
   const updateDetails = () => {
     setPerson({
       name: "Adeyemo Akanbi Aderinto",
       age: 2023 - 1940,
-      hobby: " Preaching the word of God"
+      hobby: " Preaching the word of God",
+      occupation: "University Administrator",
+      nextOfKin: "John T Craig",
+      securityClearance: false
     })
   }
 
-  const { name, age, hobby } = person;
+  const { name, age, hobby, securityClearance, nextOfKin, occupation } = person;
 
+  console.log(name)
+  console.log(occupation)
+  console.log(nextOfKin)
 
 
   return (
@@ -28,8 +37,11 @@ export function App() {
       <Starter />
       <div>
         <h1>{name}</h1>
-        <h2>{age} old this year</h2>
+        <h2>{age}</h2>
+        <h3>Is security cleared: {securityClearance}</h3>
         <p>Enjoys:{hobby}</p>
+        <h4>Works as a {occupation}</h4>
+        <p>Next of kin is : {nextOfKin}</p>
       </div>
       <button type="button" className="btn" onClick={updateDetails} >Click Me</button>
 
