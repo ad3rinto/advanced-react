73467265e2b6fe054cf2ee277c58b64bb5c957bb
ad3rinto <aad3rinto@gmail.com>
2023-03-17 13:03:55 +0000

    use effect practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,23 @@
 import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+const UseEffectBasics = () => {
+  const [value, setValue] = useState(0);
+  const sayHello = () => {
+    console.log("say Hello");
 
+  };
+  sayHello();
 
-export function App() {
-
-  const [person, setPerson] = useState({
-    name: "Dominic Longstaff",
-    age: 54
-
-  })
-
-  const updateDetails = () => {
-    setPerson({
-      name: "Professor Edwin Young",
-      age: 2016 - 1940
-    })
-  }
-
-  const { name, age, hobby, securityClearance, nextOfKin, occupation } = person;
-
-  console.log(name)
-  console.log(occupation)
-  console.log(nextOfKin)
-
-
+  useEffect(() => {
+    console.log("hello from use effect");
+  });
   return (
-    <div className='container'>
-      <Starter />
-      <div>
-        <h1>{name}</h1>
-        <h2>{age}</h2>
-
-      </div>
-      <button type="button" className="btn" onClick={updateDetails} >Click Me</button>
-
+    <div className="container">
+      <h1>Value: {value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>Click Me</button>
     </div>
   )
 };
+
+export { UseEffectBasics };
