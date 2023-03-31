9bf0c2b929b1a31c2ea26aa481e17c8e132ec0f1
ad3rinto <aad3rinto@gmail.com>
2023-03-31 21:17:10 +0100

    update code to show multiple useEffect functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics"
+import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects"
 import { useState, useEffect } from "react";
 
-const UseEffectBasics = () => {
-  const [value, setValue] = useState(0);
-  const sayHello = () => {
-    console.log("say Hello");
+const App = () => {
+  //   const [value, setValue] = useState(0);
+  //   const sayHello = () => {
+  //     console.log("say Hello");
 
-  };
-  sayHello();
+  //   };
+  //   sayHello();
 
-  useEffect(() => {
-    console.log("hello from use effect");
-  }, []);
+  //   useEffect(() => {
+  //     console.log("hello from use effect");
+  //   }, []);
   return (
     <div className="container">
-      <h1>Value: {value}</h1>
-      <button className="btn" onClick={() => setValue(value + 1)}>Click Me</button>
+      {/* <h1>Value: {value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>Click Me</button> */}
+      <Starter />
     </div>
   )
 };
 
-export { UseEffectBasics };
+export default App;
