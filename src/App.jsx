f20e70a7e3b7a55c343aba9dc9d59870c806304c
ad3rinto <aad3rinto@gmail.com>
2023-03-31 22:54:39 +0100

    set up fetching data from github and displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects"
+import Starter from "./tutorial/02-useEffect/starter/04-fetch-data"
 import { useState, useEffect } from "react";
 
 const App = () => {
--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,33 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from "react";
+
+
+
+
+
+
+
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://api.github.com/users';
+
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => setUsers(data)).then(console.log(users))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    users.map((user) => (
+      <>
+        <h2> {user.login} </h2>
+        <img src={user.avatar_url} width={500} />
+      </>
+    ))
+
+
+  )
 };
 export default FetchData;
