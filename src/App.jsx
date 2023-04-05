368f9acaed1fdc8aebb45d1820a3edf9193c4207
ad3rinto <aad3rinto@gmail.com>
2023-04-05 17:25:11 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge"
+import Navbar from "./tutorial/04-project-structure/final/Navbar/Navbar";
 
-import { useState } from "react";
 
 const App = () => {
-  const [user, setUser] = useState(null);
 
-  const login = () => {
-    //normally obtained via db or API connection
-    setUser({ name: "Vegetarian" });
-  };
-
-  const logout = () => {
-    setUser(null)
-  }
 
 
   return (
     <div className="container">
-      <Starter />
-      {user ?
-        (
-          <div>
-            <h4>Hello there {user.name}</h4>
-            <button className="btn btn-primary" onClick={logout}>Logout</button>
-          </div>
-
-
-        ) : (
-          <div>
-            <h4>Please login</h4>
-            <button className="btn btn-primary" onClick={login}>Login</button>
-          </div>
-        )}
-
+      <Navbar />
     </div>
   )
 };
